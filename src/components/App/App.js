6ef3fc23b10092482741ddf7768949edf87bee1f
Shobhit Agarwal <shobhit657@gmail.com>
2017-05-02 10:06:06 -0500

    test something

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import AddToDo from '../containers/AddToDo'
 import ToDoList from '../containers/ToDoList'
 import './App.css'
-
+// TODO Write test case for it
 const App = ({ className }) => (
   <div className={className}>
     <AddToDo />
